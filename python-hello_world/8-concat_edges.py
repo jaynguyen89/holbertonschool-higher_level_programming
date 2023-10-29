c74e768fd640f9d077eba56a93e1eb74fac50997
Jay Nguyen <jay.nguyen.developer@gmail.com>
2023-10-29 16:40:23 +1100

    Task 6 use + operator

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-print(f"{str[str.index('object'):str.index('language')]}{str[str.index('with'):str.index('very')]}{str[0:6]}")
+print(str[str.index('object'):str.index('language')] + str[str.index('with'):str.index('very')] + str[0:6])
