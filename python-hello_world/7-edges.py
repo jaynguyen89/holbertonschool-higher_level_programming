44a666b8ecb90d114a326d793633ccd1f9619cd5
Jay Nguyen <jay.nguyen.developer@gmail.com>
2023-10-29 16:01:38 +1100

    Task 5

--- python-hello_world/7-edges.py
+++ python-hello_world/7-edges.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
+
 word = "Holberton"
-# YOUR CODE GOES HERE. PLEASE REMOVE THIS LINE
-print(f"First 3 letters: {word_first_3}")
-print(f"Last 2 letters: {word_last_2}")
-print(f"Middle word: {middle_word}")
+
+print(f"First 3 letters: {word[0:3]}")
+print(f"Last 2 letters: {word[-2:]}")
+print(f"Middle word: {word[1:-1]}")
