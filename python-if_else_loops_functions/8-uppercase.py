49bd18deb6e5300dda0b31197a677403cc8d0fa5
Jay Nguyen <jay.nguyen.developer@gmail.com>
2023-10-29 20:38:39 +1100

    ???

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 def uppercase(str):
+    res = []
     for c in str:
         chcode = ord(c)
         if 97 <= chcode <= 122:
-            lchcode = chcode - 32
-            print("{0}".format(chr(lchcode)))
+            res.append(chr(chcode - 32))
         else:
-            print("{0}".format(c))
+            res.append(c)
+
+    print("{0}".format("".join(res)))
