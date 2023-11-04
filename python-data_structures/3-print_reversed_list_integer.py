622c9f67eebef78367aa25a78b9e4a7033a632f8
Jay Nguyen <jay.nguyen.developer@gmail.com>
2023-11-04 22:54:44 +1100

    3 else

--- python-data_structures/3-print_reversed_list_integer.py
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -2,7 +2,7 @@
 def print_reversed_list_integer(my_list=[]):
     if len(my_list) == 0:
         print("")
-
-    my_list.reverse()
-    for i in my_list:
-        print("{:d}".format(i))
+    else:
+        my_list.reverse()
+        for i in my_list:
+            print("{:d}".format(i))
