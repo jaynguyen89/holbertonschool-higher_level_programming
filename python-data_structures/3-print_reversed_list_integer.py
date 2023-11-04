d0dfe159f4bc0f881752eaba162d379842c50203
Jay Nguyen <jay.nguyen.developer@gmail.com>
2023-11-04 22:55:56 +1100

    3 none

--- python-data_structures/3-print_reversed_list_integer.py
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    if len(my_list) == 0:
-        print("")
+    if my_list is None or len(my_list) == 0:
+        print("", end='')
     else:
         my_list.reverse()
         for i in my_list:
