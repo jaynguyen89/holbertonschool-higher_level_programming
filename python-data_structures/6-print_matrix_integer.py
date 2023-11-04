5e24de71300c89784a3825075e11c7592ca49138
Jay Nguyen <jay.nguyen.developer@gmail.com>
2023-11-04 22:50:17 +1100

    print matrix

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -3,5 +3,5 @@ def print_matrix_integer(matrix=[[]]):
     for ls in matrix:
         for i in range(0, len(ls)):
             ending = " " if i < len(ls) - 1 else ""
-            print("{0}{1}".format(ls[i], ending), end='')
+            print("{:d}{}".format(ls[i], ending), end='')
         print("")
