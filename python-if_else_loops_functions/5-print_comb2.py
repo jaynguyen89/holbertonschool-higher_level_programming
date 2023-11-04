cd0f0c7f240b62b3fb3ba6a7a31f193f5bcf86fa
Jay Nguyen <jay.nguyen.developer@gmail.com>
2023-11-04 19:58:15 +1100

    5 print comb2

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for num in range(0, 100):
     fnum = "{:02d}".format(num)
-    print("{0}{1}".format(fnum, '' if num == 99 else ', '), end='')
+    print("{0}{1}".format(fnum, '' if num == 99 else ', '))
