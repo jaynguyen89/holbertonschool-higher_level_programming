01ba667a140907d38f699ce97d680d4291197283
Jay Nguyen <jay.nguyen.developer@gmail.com>
2023-11-04 21:17:08 +1100

    fix long line

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -3,6 +3,8 @@ import sys
 
 if __name__ == '__main__':
     numOfArgs = len(sys.argv) - 1
-    print(f"{numOfArgs} {'argument' if numOfArgs == 1 else 'arguments'}{'.' if numOfArgs == 0 else ':'}")
+    argText = 'argument' if numOfArgs == 1 else 'arguments'
+    ending = '.' if numOfArgs == 0 else ':'
+    print(f"{numOfArgs} {argText}{ending}")
     for i in range(1, numOfArgs + 1):
         print("{0}: {1}".format(i, sys.argv[i]))
