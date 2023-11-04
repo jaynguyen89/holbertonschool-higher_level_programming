c1f5cfd7ffbd24064d933476458ada4f046122d0
Jay Nguyen <jay.nguyen.developer@gmail.com>
2023-11-04 22:51:24 +1100

    no c

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -6,8 +6,3 @@ def no_c(my_string):
             ls[ls.index(char)] = ''
 
     return "".join(ls)
-
-if __name__ == '__main__':
-    print(no_c("Best School"))
-    print(no_c("Chicago"))
-    print(no_c("C is fun!"))
