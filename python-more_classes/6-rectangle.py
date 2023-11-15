10fa07773674a9c26503999a28e3cbcd99a5bdb2
Jay Nguyen <jay.nguyen.developer@gmail.com>
2023-11-16 01:38:06 +1100

    ValueError

--- python-more_classes/6-rectangle.py
+++ python-more_classes/6-rectangle.py
@@ -23,7 +23,7 @@ class Rectangle:
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         elif value < 0:
-            raise TypeError("width must be >= 0")
+            raise ValueError("width must be >= 0")
         else:
             self.__width = value
 
@@ -38,7 +38,7 @@ class Rectangle:
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
         elif value < 0:
-            raise TypeError("height must be >= 0")
+            raise ValueError("height must be >= 0")
         else:
             self.__height = value
 
