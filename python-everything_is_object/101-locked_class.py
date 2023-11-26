b411b51fa1a27c713dbc96c0eb94bc5efb05f469
Jay Nguyen <jay.nguyen.developer@gmail.com>
2023-11-26 22:08:48 +1100

    doc

--- python-everything_is_object/101-locked_class.py
+++ python-everything_is_object/101-locked_class.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
+"""document"""
+
+
 class LockedClass:
+    """document"""
+
     def __setattr__(self, key, value):
+        """document"""
         if key != "first_name":
             raise AttributeError(f"'LockedClass' object has no attribute '{key}'")
         else:
