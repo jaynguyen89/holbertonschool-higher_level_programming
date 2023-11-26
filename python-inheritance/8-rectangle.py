4ef3a3a53aac8eeb6aa3ed664139e3e0c49cc2b4
Jay Nguyen <jay.nguyen.developer@gmail.com>
2023-11-27 01:29:56 +1100

    docs

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
-BaseGeometry = __import__('7-base_geometry').BaseGeometry
-
 """document"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
     """document"""
 
     def __init__(self, width, height):
+        """document"""
         super().integer_validator("width", width)
         super().integer_validator("height", height)
         self._width = width
--- python-inheritance/9-rectangle.py
+++ python-inheritance/9-rectangle.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-BaseGeometry = __import__('7-base_geometry').BaseGeometry
-
 """document"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
